201d2df18047f3797cc20956e12558fed5f33990
Esther Turin <turinesther@gmail.com>
2022-05-08 19:08:30 -0400

    Add project files.

---
+++ PeopleTable.Data/PeopleDataContext.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace PeopleTable.Data
+{
+    
+        public class PeopleDataContext : DbContext
+        {
+            private string _connectionString;
+            public PeopleDataContext(string connectionString)
+            {
+                _connectionString = connectionString;
+            }
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer(_connectionString);
+            }
+            public DbSet<Person> People { get; set; }
+        }
+    }
---
+++ PeopleTable.Data/PeopleRepo.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PeopleTable.Data
+{
+    class PeopleRepo
+    {
+
+        private string _connectionString;
+        public PeopleRepo(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        public List<Person> GetAll()
+        {
+            using var context = new PeopleDataContext(_connectionString);
+            return context.People.ToList();
+        }
+        public void AddPerson(Person person)
+        {
+            using var context = new PeopleDataContext(_connectionString);
+            context.People.Add(person);
+            context.SaveChanges();
+        }
+        public void DeletePerson(Person person)
+        {
+            using var context = new PeopleDataContext(_connectionString);
+            context.Database.ExecuteSqlInterpolated($"DELETE FROM People WHERE Id= {person.Id}");
+            context.SaveChanges();
+
+        }
+
+        public void UpdatePerson(Person person)
+        {
+            using var context = new PeopleDataContext(_connectionString);
+
+            context.Database.ExecuteSqlInterpolated($"UPDATE People Set FirstName={person.FirstName},  LastName={person.LastName}, Age={person.Age} Where Id={person.Id}");
+            context.SaveChanges();
+        }
+
+    }
+}
